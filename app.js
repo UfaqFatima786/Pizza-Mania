6fa664907c8b001d02d5a4f75377bbb17e16cd86
UfaqFatima786 <u.capricorn19@gmail.com>
2026-07-08 02:06:49 +0500

    pizza

--- app.js
+++ app.js
@@ -1,18 +1,90 @@
+const offerButtons=document.querySelectorAll(".offer-btn");
 
-// const cards=document.querySelectorAll(".why-card");
+offerButtons.forEach(button=>{
 
-// cards.forEach(card=>{
+button.addEventListener("click",()=>{
 
-// card.addEventListener("mouseenter",()=>{
+button.innerHTML="✔ Claimed";
 
-// card.style.transform="translateY(-15px) scale(1.03)";
+button.style.background="#28a745";
 
-// })
+setTimeout(()=>{
 
-// card.addEventListener("mouseleave",()=>{
+button.innerHTML="Order Now";
 
-// card.style.transform="translateY(0px) scale(1)";
+button.style.background="#d62828";
 
-// })
+},2000);
 
-// });
+});
+
+});
+
+const offerCards=document.querySelectorAll(".offer-card");
+offerCards.forEach(card=>{
+
+card.addEventListener("mousemove",(e)=>{
+
+const x=e.offsetX/15;
+const y=e.offsetY/15;
+
+card.style.transform=`rotateX(${y}deg) rotateY(${x}deg)`;
+
+});
+
+card.addEventListener("mouseleave",()=>{
+
+card.style.transform="rotateX(0deg) rotateY(0deg)";
+
+});
+
+});
+
+const reviews = document.querySelectorAll(".review-card");
+
+let currentReview = 0;
+
+function showReview(index){
+
+    reviews.forEach((review)=>{
+
+        review.classList.remove("active");
+
+    });
+
+    reviews[index].classList.add("active");
+
+}
+
+setInterval(()=>{
+
+    currentReview++;
+
+    if(currentReview >= reviews.length){
+
+        currentReview = 0;
+
+    }
+
+    showReview(currentReview);
+
+},3000);
+
+
+reviews.forEach((card)=>{
+
+    card.addEventListener("mouseenter",()=>{
+
+        card.style.transform="translateY(-12px)";
+
+        card.style.transition=".4s";
+
+    });
+
+    card.addEventListener("mouseleave",()=>{
+
+        card.style.transform="translateY(0px)";
+
+    });
+
+});
