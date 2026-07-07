d13749e96ed846ed0bf5f4cbfc813f863180f619
UfaqFatima786 <u.capricorn19@gmail.com>
2026-07-08 00:58:35 +0500

    pizza

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// const cards=document.querySelectorAll(".why-card");
+
+// cards.forEach(card=>{
+
+// card.addEventListener("mouseenter",()=>{
+
+// card.style.transform="translateY(-15px) scale(1.03)";
+
+// })
+
+// card.addEventListener("mouseleave",()=>{
+
+// card.style.transform="translateY(0px) scale(1)";
+
+// })
+
+// });
